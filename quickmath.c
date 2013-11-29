58bd120b97a59d8bbe3d20d7b69d5534764beef2
James Atwood <jatwood@cs.umass.edu>
2013-11-29 10:55:51 -0500

    correct pareto settings, less complicated than I initially thought

--- quickmath.c
+++ quickmath.c
@@ -65,15 +65,17 @@ rand_init(char *base_dir, char *seed_name) {
 double
 sample_fitness_pareto_in(double lambda, double p) {
   double xm;
-  xm = ((1. + p * lambda) / (2. + p * lambda)) * lambda;
+  xm = (lambda - 1.);
+  //xm = ((1. + p * lambda) / (2. + p * lambda)) * lambda;
   return xm / pow((((double) rand()) / RAND_MAX),1./lambda);
 }
 
 double
 sample_fitness_pareto_out(double mu, double p) {
   double xm;
-  double qm1 = (1. / (1. - p));
-  xm = ((qm1 + mu * p * qm1) / (1 + qm1 + mu * p * qm1)) * mu;
+  //double qm1 = (1. / (1. - p));
+  xm = (mu - 1.);
+  //xm = ((qm1 + mu * p * qm1) / (1 + qm1 + mu * p * qm1)) * mu;
   //xm = ( (1. / (1. - p + p * lambda) / (2. + p * lambda)) * lambda;
   return xm / pow((((double) rand()) / RAND_MAX),1./mu);
 }
